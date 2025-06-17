621b46024e6433cd2148d725dedab55af1380552
Wendel <wendel.m.veloso@hotmail.com>
2025-06-17 15:05:32 -0300

    feat: implement getAllTitles(1)

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,9 +1,11 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
-}
+  return movies.map((element) => element.titulo);
+};
+
+console.log(getAllTitles(movies));
 
 module.exports = {
-  getAllTitles
-}
+  getAllTitles,
+};
