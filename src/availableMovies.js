16fd1f15d880ff4c628be9cf10b2383bb7334c7d
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 16:15:20 -0300

    feat: implement availableMovies(7)

--- src/availableMovies.js
+++ src/availableMovies.js
@@ -1,9 +1,11 @@
 const movies = require("../data/movies");
 
 const availableMovies = (movies) => {
- 
-}
+  return movies
+    .filter((element) => element.disponivel === true)
+    .map((element) => element.titulo);
+};
 
 module.exports = {
-  availableMovies
-}
+  availableMovies,
+};
