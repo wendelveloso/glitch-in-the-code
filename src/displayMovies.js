89d81befc58ec6fc6ebb1e6970063c5296ac41ba
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 15:50:07 -0300

    feat: implement displayMovies(5)

--- src/displayMovies.js
+++ src/displayMovies.js
@@ -1,9 +1,11 @@
-const { movies } = require("../data/list1");
+const { list1 } = require("../data/list1");
 
 const displayMovies = (movies) => {
- 
-}
+  movies.map((element) => {
+    console.log(`Título: ${element.titulo} - Diretor: ${element.diretor}`);
+  });
+};
 
 module.exports = {
-  displayMovies
-}
+  displayMovies,
+};
