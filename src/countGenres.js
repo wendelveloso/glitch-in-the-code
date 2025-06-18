baa4a1e70754eafcc8c9731a5136337113f2897a
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 17:25:50 -0300

    feat: implement countGenres(10)

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,9 +1,19 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  return movies
+    .map((element) => element.genero)
+    .flat()
+    .reduce((todosGeneros, genero) => {
+      if (genero in todosGeneros) {
+        todosGeneros[genero]++;
+      } else {
+        todosGeneros[genero] = 1;
+      }
+      return todosGeneros;
+    }, {});
 }
 
 module.exports = {
-  countGenres
-}
+  countGenres,
+};
