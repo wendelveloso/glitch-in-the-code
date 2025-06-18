acdac86729e508164d580be80af7f14a00eeb6cc
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 16:28:46 -0300

    feat: implement nolanMoviesByRating(8)

--- src/nolanMoviesByRating.js
+++ src/nolanMoviesByRating.js
@@ -1,9 +1,11 @@
 const movies = require("../data/movies");
 
 const nolanMoviesByRating = (movies) => {
- 
-}
+  return movies
+    .filter((element) => element.diretor.includes("Nolan"))
+    .sort((a, b) => b.avaliacao - a.avaliacao);
+};
 
 module.exports = {
-  nolanMoviesByRating
-}
+  nolanMoviesByRating,
+};
