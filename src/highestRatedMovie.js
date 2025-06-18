492709af4e730d301a0e96b322a89aab7d7a8cec
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 15:24:51 -0300

    feat: implement highestRatedMovie(4)

--- src/highestRatedMovie.js
+++ src/highestRatedMovie.js
@@ -1,9 +1,17 @@
 const movies = require("../data/movies");
 
 const highestRatedMovie = (movies) => {
-  
-}
+  if (movies.length === 0) return undefined;
+
+  return movies.reduce((maior, atual) => {
+    if (atual.avaliacao > maior.avaliacao) {
+      return atual;
+    } else {
+      return maior;
+    }
+  });
+};
 
 module.exports = {
-  highestRatedMovie
-}
+  highestRatedMovie,
+};
