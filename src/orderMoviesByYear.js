44f376db4d444a98f69798844eb352a5d98babfa
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 16:02:12 -0300

    feat: implement orderMoviesByYear(6)

--- src/orderMoviesByYear.js
+++ src/orderMoviesByYear.js
@@ -1,9 +1,10 @@
 const movies = require("../data/movies");
 
 const orderMoviesByYear = (movies) => {
-  
-}
+  const newOrderMovies = [...movies];
+  return newOrderMovies.sort((a, b) => a.anoLancamento - b.anoLancamento);
+};
 
 module.exports = {
-  orderMoviesByYear
-}
+  orderMoviesByYear,
+};
