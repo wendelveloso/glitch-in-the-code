17d47a6b583f002a841b3fe49eff9bbc40b35551
Wendel <wendel.m.veloso@hotmail.com>
2025-06-18 17:02:39 -0300

    feat: implement actorsInMovie(9)

--- src/actorsInMovie.js
+++ src/actorsInMovie.js
@@ -1,9 +1,21 @@
 const movies = require("../data/movies");
 
 const actorsInMovie = (titulo) => {
-  
-}
+  const movieFound = movies.find((element) => element.titulo === titulo);
+  if (!movieFound) {
+    return "Filme não encontrado.";
+  }
+  if (
+    !movieFound.elenco ||
+    movieFound.titulo === "" ||
+    movieFound.titulo === null
+  ) {
+    return "Não possui elenco, título é nulo ou vazio";
+  }
+
+  return movieFound.elenco.slice(0, 2);
+};
 
 module.exports = {
-  actorsInMovie
-}
+  actorsInMovie,
+};
