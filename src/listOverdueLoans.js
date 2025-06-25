70a390c75ff16b29f55e62eb57ec72e3ca7f3bf0
Wendel <wendel.m.veloso@hotmail.com>
2025-06-25 17:46:37 -0300

    feat: implement listOverdueLoans(13)

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,9 +1,27 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+  const atrasados = [];
+
+  movies.forEach((filme) => {
+    filme.emprestimos.forEach((emprestimo) => {
+      const dataDevolucao = new Date(emprestimo.dataDevolucao);
+
+      if (dataDevolucao < new Date()) {
+        atrasados.push({
+          id: filme.id,
+          titulo: filme.titulo,
+          dataEmprestimo: emprestimo.dataEmprestimo,
+          dataDevolucao: emprestimo.dataDevolucao,
+          status: "atrasado",
+        });
+      }
+    });
+  });
+
+  return atrasados;
 }
 
 module.exports = {
-  listOverdueLoans
-}
+  listOverdueLoans,
+};
